1f5e2c8572de4c277d41000de261368da943ba72
Astrum <kts6481@gmail.com>
2023-05-23 10:32:11 +0900

    update collect and print review info

--- Collector/collect_movie_review.py
+++ Collector/collect_movie_review.py
@@ -1,4 +1,7 @@
 import requests
+import math
+import time
+import re
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -26,6 +29,50 @@ driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),optio
 #ftp : 파일 전송
 #ssh : 터미널 접속
 #smtp : 메일 전송
+
+
+###############################
+# 2. Opne URL in ChromeDriver #
+###############################
+
 movie_id = 160244
 url=f"https://movie.daum.net/moviedb/grade?movieId={movie_id}"
 
+#Selenium의 ChromeDriver를 통해서 해당 URl 접속
+driver.get(url)
+time.sleep(1)  # 딜레이 1초(웹페이지 렌더링 완료까지 기다리기
+doc_html = driver.page_source
+
+doc = BeautifulSoup(doc_html, "html.parser")
+title = doc.select("span.txt_tit")[1].text.strip()  #영화 제목 수정
+
+total_review_txt = doc.select("span.txt_netizen")[0].text
+# 정규화 -> 숫자만 추출
+total_review = int(re.sub(r"[^~0-9]","",total_review_txt))
+print(total_review)
+
+click_cnt = math.ceil((total_review - 10) / 30)
+
+for i in range(click_cnt):
+    #평점 더보기 버튼 클릭(리뷰 30개씩 증가함)
+    driver.find_element(By.CLASS_NAME, "link_fold").click()
+    time.sleep(1)
+time.sleep(5)
+#해당 페이지의 모든 리뷰 출력 완료
+review_html = driver.page_source
+doc = BeautifulSoup(review_html, "html.parser")
+review_list = doc.select("ul.list_comment div.cmt_info")
+
+for review_box in review_list:
+    score = review_box.select("div.ratings")[0].text
+    writer = review_box.select("span")[0].text
+    temp=writer.find(",")
+    review_date = review_box.select("span.txt_date")[0].text
+    review = review_box.select("p.desc_txt")[0].text
+
+    print(f" - 평점 : {score}")
+    print(f" - 작성자 : {writer[6:temp]}")
+    print(f" - 작성일자 : {review_date[:-6]}")
+    print(f" -----리뷰-----\n{review}")
+    print("-"*100)
+    #
